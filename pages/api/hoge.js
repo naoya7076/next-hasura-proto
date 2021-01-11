482ed2d957c665b155fafea799ceaaad6bab029d
naoya7076 <naoya7076@gmail.com>
2021-01-11 23:38:30 +0900

    3章の修正

--- pages/api/hoge.js
+++
@@ -1,5 +0,0 @@
-export default(req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-type', 'application/json')
-  res.end(JSON.stringify({hoge:'fuga'}))
-}
--- pages/index.js
+++
@@ -1,6 +0,0 @@
-import Head from 'next/head'
-import styles from './index.module.css'
-
-export default function Home() {
-  return <div className={styles.error}>error!</div>
-}
