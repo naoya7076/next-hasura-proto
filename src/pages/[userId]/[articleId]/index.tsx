7ce88b03b80af21f4d050408d49e01785c44769b
naoya7076 <naoya7076@gmail.com>
2021-01-20 22:46:09 +0900

    見た目を整えるその1

--- src/pages/[userId]/[articleId]/index.tsx
+++ src/pages/[userId]/[articleId]/index.tsx
@@ -1,7 +1,10 @@
 import { useRouter } from 'next/router';
 import { NextPage } from 'next';
+import Error from 'next/error';
 
 import { useGetArticleQuery } from '@/generated/graphql';
+
+import styles from './index.module.css';
 
 const ArticlePage: NextPage = () => {
   const router = useRouter();
@@ -12,14 +15,23 @@ const ArticlePage: NextPage = () => {
       id: articleId as string,
     },
   });
-
   if (loading) {
     return <p>...loading</p>;
   }
   if (error) {
     return <p>{error.toString()}</p>;
   }
-  return <div>{JSON.stringify(data)}</div>;
-};
+  if (!data || !data.articles_by_pk) {
+    return <Error statusCode={404} />;
+  }
 
+  const { user, subject, content } = data.articles_by_pk;
+  return (
+    <div className={styles.contentContainer}>
+      <div>{JSON.stringify(user)}</div>
+      <div>{subject}</div>
+      <div>{content}</div>
+    </div>
+  );
+};
 export default ArticlePage;
